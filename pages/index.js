b2472076510ac998654dc7c21004c30800a41f28
elmondino <elmonds@hotmail.co.uk>
2021-10-28 09:15:27 +0100

    removal of extra data

--- pages/index.js
+++ pages/index.js
@@ -57,36 +57,6 @@ export default function Home() {
       title: "Sixth event",
       description: "Third event is currently taking place.",
     },
-    {
-      time: "17:33",
-      title: "Most immediate",
-      description: "Description about what just happened",
-    },
-    {
-      time: "17:11",
-      title: "Yet another",
-      description: "Chronologically, the third in the timeline.",
-    },
-    {
-      time: "16:52",
-      title: "Third event",
-      description: "Some more stuff is going on.",
-    },
-    {
-      time: "16:16",
-      title: "Fourth event",
-      description: "Some new development is taking place.",
-    },
-    {
-      time: "15:45",
-      title: "Random event",
-      description: "Another random event is taking place.",
-    },
-    {
-      time: "15:21",
-      title: "Sixth event",
-      description: "Third event is currently taking place.",
-    },
   ];
 
   return <Timeline data={data} />;
